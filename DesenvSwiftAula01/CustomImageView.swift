1a7745de0f69f92663163d4895d5721998fd1d32
Usuário Convidado <Guest@PA06MICRO16.local>
2018-03-24 12:31:01 -0300

    Salvando ...

--- DesenvSwiftAula01/CustomImageView.swift
+++ DesenvSwiftAula01/CustomImageView.swift
@@ -8,14 +8,24 @@
 
 import UIKit
 
-class CustomImageView: UIImageView {
+@IBDesignable class CustomImageView: UIImageView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
     }
-    */
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = .white {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
 
 }
